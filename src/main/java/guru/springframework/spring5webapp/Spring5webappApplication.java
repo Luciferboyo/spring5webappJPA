b61452df73fe21bc88a58f34537fa19fb926d31c
boyo <2575133589@qq.com>
2025-09-09 15:30:27 +0530

    initialization

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -3,6 +3,9 @@ package guru.springframework.spring5webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author boyo
+ */
 @SpringBootApplication
 public class Spring5webappApplication {
 
