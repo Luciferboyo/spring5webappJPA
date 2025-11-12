225eaca682987a951cf26d55c4fb6251249c3e1d
boyo <2575133589@qq.com>
2025-11-12 08:52:32 +0530

    create "author" table

---
+++ src/main/java/guru/springframework/spring5webapp/entity/Author.java
@@ -0,0 +1,20 @@
+package guru.springframework.spring5webapp.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author boyo
+ */
+public class Author {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+}
